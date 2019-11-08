adfc8794b08fb51e888cdbec9c79964bb090d08c
danielwalker <dawalker2017@outlook.com>
2019-11-08 07:17:05 -0500

    Added <dashboard.html> into urls.

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.dashboard, name="dashboard"),
+    # path('', TemplateView.as_view(template_name="core/login.html"), name='login'),
+    path('admin/', admin.site.urls)
 ]
