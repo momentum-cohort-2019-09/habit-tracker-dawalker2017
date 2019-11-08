ecf7567de2578fe5fd9f9c4b1bcc3ed0d5756c1a
danielwalker <dawalker2017@outlook.com>
2019-11-08 07:17:42 -0500

    Created a User and Habit model.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#===========================================| User class |=========================================#
+class User(AbstractUser):
+    pass
+
+#===========================================| Habit class |=========================================#
+class Habit(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="habits",
+        )
+
+    subject = models.CharField(
+        max_length=25,
+        verbose_name="Subject:",
+        blank=False,
+        null=True,
+        help_text="Max Characters: 25"
+        )
+
+    description = models.CharField(
+        max_length=255,
+        verbose_name="Description:",
+        blank=False,
+        null=True,
+        help_text="Max Characters: 255",
+        )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        )
+
+    is_active = models.BooleanField(
+        default=True,
+        )
+
+    def __str__(self):
+        return self.subject
+    def __str__(self):
+        return self.description
+    def __str__(self):
+        return self.created_at
+    def __str__(self):
+        return self.updated_at
+    def __str__(self):
+        return self.is_active
