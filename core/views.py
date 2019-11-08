8356d387918d705f6aa6de5bd0c73bc081d8111a
danielwalker <dawalker2017@outlook.com>
2019-11-08 07:19:54 -0500

    Created view for <dashboard.html>

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from core.models import User, Habit
 
-# Create your views here.
+# def login(request):
+#     return render(request, "core/login.html")
+
+def dashboard(request):
+    return render(request, "core/dashboard.html")
